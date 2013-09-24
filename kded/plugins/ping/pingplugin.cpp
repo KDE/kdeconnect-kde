95ef31acea3892268e553b2a5f284412a4f954ee
Albert Vaca <albertvaka@gmail.com>
2013-09-24 14:14:34 +0200

    Documentation
    Added a README to each plugin explaining the content of the packages they
    send and receive
    Explained the format of encrypted packages.

--- kded/plugins/ping/pingplugin.cpp
+++ kded/plugins/ping/pingplugin.cpp
@@ -49,7 +49,7 @@ bool PingPlugin::receivePackage(const NetworkPackage& np)
     notification->setPixmap(KIcon("dialog-ok").pixmap(48, 48));
     notification->setComponentData(KComponentData("kdeconnect", "kdeconnect"));
     notification->setTitle(device()->name());
-    notification->setText(np.get<QString>("message",i18n("Ping!")));
+    notification->setText(np.get<QString>("message",i18n("Ping!"))); //This can be a source of spam
     notification->sendEvent();
 
     return true;
