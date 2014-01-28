5a8d7bbf459acf1f2ed1c5d8d6b7e56040032a80
Lukáš Tinkl <lukas@kde.org>
2014-01-28 02:07:10 +0100

    the catalog name must follow the plasma_applet_<name> pattern
    otherwise it's just wasting the translators' time... sigh

--- plasmoid/Messages.sh
+++ plasmoid/Messages.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 #.qml
-$XGETTEXT `find package -name '*.qml'` -L Java -o $podir/kdeconnect-plasmoid.pot
+$XGETTEXT `find package -name '*.qml'` -L Java -o $podir/plasma_applet_kdeconnect.pot
 
 #.ui (-j passed to merge into existing file)
 $EXTRACTRC `find -name '*.ui' -o -name '*.rc'` >> rc.cpp
-$XGETTEXT rc.cpp -o -j $podir/kdeconnect-plasmoid.pot
+$XGETTEXT rc.cpp -o -j $podir/plasma_applet_kdeconnect.pot
 rm -f rc.cpp
-
-
