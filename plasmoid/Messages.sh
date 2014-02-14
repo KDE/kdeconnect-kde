d37a2259ff1266829867e5a0deb39b51a389297c
Burkhard Lück <lueck@hube-lueck.de>
2014-02-14 17:44:12 +0100

    fix broken message extraction, so ui/rc files any more

--- plasmoid/Messages.sh
+++ plasmoid/Messages.sh
@@ -2,8 +2,3 @@
 
 #.qml
 $XGETTEXT `find package -name '*.qml'` -L Java -o $podir/plasma_applet_kdeconnect.pot
-
-#.ui (-j passed to merge into existing file)
-$EXTRACTRC `find -name '*.ui' -o -name '*.rc'` >> rc.cpp
-$XGETTEXT rc.cpp -o -j $podir/plasma_applet_kdeconnect.pot
-rm -f rc.cpp
