d7e9a42a7bfcbea1910fffb012cc49c15f9d5248
Albert Vaca <albertvaka@gmail.com>
2014-04-12 23:04:35 +0200

    Moved unit tests together

--- tests/testsocketlinereader.cpp
+++ tests/testsocketlinereader.cpp
@@ -16,7 +16,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA       *
  *************************************************************************************/
 
-#include "../backends/lan/socketlinereader.h"
+#include "../kded/backends/lan/socketlinereader.h"
 
 #include <QTest>
 #include <QTcpServer>
