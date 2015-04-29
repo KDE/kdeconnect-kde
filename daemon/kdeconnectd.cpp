e34a29103cf7462435ef5b6b5a57946c5f8a3d0d
Aleix Pol <aleixpol@kde.org>
2015-04-29 02:25:49 +0200

    Define interface name on the leaf class
    Otherwise it uses org.kde.kdeconnect.DesktopDaemon and signals are not
    properly propagated.

--- daemon/kdeconnectd.cpp
+++ daemon/kdeconnectd.cpp
@@ -64,6 +64,7 @@ void initializeTermHandlers(QCoreApplication* app, Daemon* daemon)
 class DesktopDaemon : public Daemon
 {
     Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.kde.kdeconnect.daemon")
 public:
     DesktopDaemon(QObject* parent = Q_NULLPTR)
         : Daemon(parent)
