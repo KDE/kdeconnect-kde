2b34d28d6f156ea9f3d08128e65db78b7159d0d6
Rex Dieter <rdieter@math.unl.edu>
2015-08-26 08:29:34 -0500

    more validation fixes

--- daemon/kdeconnectd.desktop.cmake
+++ daemon/kdeconnectd.desktop.cmake
@@ -1,10 +1,10 @@
 [Desktop Entry]
-Type=Service
+Type=Application
 Exec=${LIBEXEC_INSTALL_DIR}/kdeconnectd
 X-KDE-StartupNotify=false
 X-KDE-autostart-phase=0
 X-GNOME-Autostart-enabled=true
-OnlyShowIn=KDE;GNOME;Unity;XFCE
+OnlyShowIn=KDE;GNOME;Unity;XFCE;
 NoDisplay=true
 
 Name=KDEConnect daemon
