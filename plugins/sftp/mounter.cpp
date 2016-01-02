36e62891ca8062344d01c53d7dc5c91cb699690b
Albert Vaca <albertvaka@gmail.com>
2016-01-02 12:30:50 -0800

    This syntax also works on older OpenSSH versions.
    Like the one that Ubuntu distributes
    
    BUG: 355015

--- plugins/sftp/mounter.cpp
+++ plugins/sftp/mounter.cpp
@@ -123,7 +123,7 @@ void Mounter::onPakcageReceived(const NetworkPackage& np)
         << "-o" << "IdentityFile=" + KdeConnectConfig::instance()->privateKeyPath()
         << "-o" << "StrictHostKeyChecking=no" //Do not ask for confirmation because it is not a known host
         << "-o" << "UserKnownHostsFile=/dev/null" //Prevent storing as a known host
-        << "-o" << "HostKeyAlgorithms=+ssh-dss"; //https://bugs.kde.org/show_bug.cgi?id=351725
+        << "-o" << "HostKeyAlgorithms=ssh-dss"; //https://bugs.kde.org/show_bug.cgi?id=351725
 
     m_proc->setProgram(program, arguments);
 
