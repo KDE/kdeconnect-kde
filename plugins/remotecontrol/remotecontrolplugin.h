4d1cf586120d652d5ed1218c320271d4df1b31d4
Aleix Pol <aleixpol@kde.org>
2016-06-05 23:08:47 +0200

    Reflect to changes in the json file
    We shouldn't have changed mousepad -> input, reverting

--- plugins/remotecontrol/remotecontrolplugin.h
+++ plugins/remotecontrol/remotecontrolplugin.h
@@ -25,7 +25,7 @@
 
 #include <core/kdeconnectplugin.h>
 
-#define PACKAGE_TYPE_INPUT_REQUEST QLatin1String("kdeconnect.input.request")
+#define PACKAGE_TYPE_MOUSEPAD_REQUEST QLatin1String("kdeconnect.mousepad.request")
 
 class Q_DECL_EXPORT RemoteControlPlugin
     : public KdeConnectPlugin
