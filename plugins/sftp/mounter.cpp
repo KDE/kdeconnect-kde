9bfa255a60b25c0976ec6062e2252eb67b24dca5
Albert Vaca <albertvaka@gmail.com>
2018-09-04 14:40:42 +0200

    It's DSS we want to enable
    My previous commit changed it to RSA by mistake

--- plugins/sftp/mounter.cpp
+++ plugins/sftp/mounter.cpp
@@ -134,7 +134,7 @@ void Mounter::onPakcageReceived(const NetworkPacket& np)
         << QStringLiteral("-o") << "IdentityFile=" + KdeConnectConfig::instance()->privateKeyPath()
         << QStringLiteral("-o") << QStringLiteral("StrictHostKeyChecking=no") //Do not ask for confirmation because it is not a known host
         << QStringLiteral("-o") << QStringLiteral("UserKnownHostsFile=/dev/null") //Prevent storing as a known host
-        << QStringLiteral("-o") << QStringLiteral("HostKeyAlgorithms=+ssh-rsa") //https://bugs.kde.org/show_bug.cgi?id=351725
+        << QStringLiteral("-o") << QStringLiteral("HostKeyAlgorithms=+ssh-dss") //https://bugs.kde.org/show_bug.cgi?id=351725
         << QStringLiteral("-o") << QStringLiteral("uid=") + QString::number(getuid())
         << QStringLiteral("-o") << QStringLiteral("gid=") + QString::number(getgid())
         << QStringLiteral("-o") << QStringLiteral("reconnect")
