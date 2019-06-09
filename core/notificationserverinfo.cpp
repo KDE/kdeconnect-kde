13230ff580a6fae27b4a6a0419f459ba1b31e729
Weixuan Xiao <veyx.shaw@gmail.com>
2019-06-09 18:45:03 +0000

    Fix missing sessionBus() in core/notificationserverinfo.cpp

--- core/notificationserverinfo.cpp
+++ core/notificationserverinfo.cpp
@@ -23,7 +23,8 @@
 #include <QDBusMessage>
 #include <QDBusPendingReply>
 #include <QDBusPendingCallWatcher>
-#include <QDBusConnection>
+
+#include "dbushelper.h"
 
 #include "core_debug.h"
 
@@ -37,7 +38,7 @@ void NotificationServerInfo::init()
 {
     QDBusMessage query = QDBusMessage::createMethodCall(QStringLiteral("org.freedesktop.Notifications"), QStringLiteral("/org/freedesktop/Notifications"), QStringLiteral("org.freedesktop.Notifications"), QStringLiteral("GetCapabilities"));
 
-    QDBusPendingReply<QStringList> reply = QDBusConnection::sessionBus().asyncCall(query);
+    QDBusPendingReply<QStringList> reply = DbusHelper::sessionBus().asyncCall(query);
     QDBusPendingCallWatcher *watcher = new QDBusPendingCallWatcher(reply, this);
     connect(watcher, &QDBusPendingCallWatcher::finished, this, [this, reply, watcher] {
         watcher->deleteLater();
