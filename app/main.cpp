245eb954d581eee416b496966fb607f4859c72a4
Nicolas Fella <nicolas.fella@gmx.de>
2019-07-21 11:02:24 +0000

    Rename kcapp to kdeconnect

--- app/main.cpp
+++ app/main.cpp
@@ -30,7 +30,7 @@
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
-    KAboutData aboutData(QStringLiteral("kdeconnect.app"), i18n("KDE Connect App"), QStringLiteral("1.0"), i18n("KDE Connect App"), KAboutLicense::GPL, i18n("(c) 2015, Aleix Pol Gonzalez"));
+    KAboutData aboutData(QStringLiteral("kdeconnect.app"), i18n("KDE Connect"), QStringLiteral("1.0"), i18n("KDE Connect"), KAboutLicense::GPL, i18n("(c) 2015, Aleix Pol Gonzalez"));
     aboutData.addAuthor(i18n("Aleix Pol Gonzalez"), i18n("Maintainer"), QStringLiteral("aleixpol@kde.org"));
     KAboutData::setApplicationData(aboutData);
 
