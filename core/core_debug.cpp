607b9a6566527eb49172311b08538ac5c757f1ae
Nicolas Fella <nicolas.fella@gmx.de>
2019-07-22 21:29:39 +0200

    Revert "Use ecm_qt_declare_logging_category"
    It's missing stuff to work properly. Revert until I figured that out
    
    This reverts commit 8a118a6fae5b83ed9ad66cc7c91d3de34e194a9f.

---
+++ core/core_debug.cpp
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2013 Albert Vaca <albertvaka@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "core_debug.h"
+
+Q_LOGGING_CATEGORY(KDECONNECT_CORE, "kdeconnect.core")
+
+#if defined(__GNU_LIBRARY__)
+#include <execinfo.h>
+#include <stdlib.h>
+#include <unistd.h>
+#endif
+
+void logBacktrace()
+{
+#if defined(__GNU_LIBRARY__)
+    void* array[32];
+    size_t size = backtrace (array, 32);
+    char** strings = backtrace_symbols (array, size);
+    backtrace_symbols_fd(array, size, STDERR_FILENO);
+    free (strings);
+#endif
+}
