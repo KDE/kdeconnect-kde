7e25e60c5f0675087d8f9bc0c603178e136936e2
Piyush Aggarwal <piyushaggarwal002@gmail.com>
2019-08-31 10:40:43 +0000

    add inhibit screensaver plugin for Windows

---
+++ plugins/screensaver-inhibit/screensaverinhibitplugin-win.cpp
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2019 Piyush Aggarwal <piyushaggarwal002@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "screensaverinhibitplugin-win.h"
+
+#include <KPluginFactory>
+#include <QLoggingCategory>
+#include <Windows.h>
+
+K_PLUGIN_CLASS_WITH_JSON(ScreensaverInhibitPlugin, "kdeconnect_screensaver_inhibit.json")
+
+Q_LOGGING_CATEGORY(KDECONNECT_PLUGIN_SCREENSAVERINHIBIT, "kdeconnect.plugin.screensaverinhibit")
+
+ScreensaverInhibitPlugin::ScreensaverInhibitPlugin(QObject* parent, const QVariantList& args)
+    : KdeConnectPlugin(parent, args)
+{
+    SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
+}
+
+ScreensaverInhibitPlugin::~ScreensaverInhibitPlugin()
+{
+    SetThreadExecutionState(ES_CONTINUOUS);
+}
+
+
+bool ScreensaverInhibitPlugin::receivePacket(const NetworkPacket& np)
+{
+    Q_UNUSED(np);
+    return false;
+}
+
+#include "screensaverinhibitplugin-win.moc"
