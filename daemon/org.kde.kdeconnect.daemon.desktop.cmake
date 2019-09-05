8d6136f9530c32a5d93d122f4896b79401a2776f
Aleix Pol <aleixpol@kde.org>
2019-09-05 16:57:38 +0200

    wayland: we need the input interface to be able to use the remote capabilities

--- daemon/org.kde.kdeconnect.daemon.desktop.cmake
+++ daemon/org.kde.kdeconnect.daemon.desktop.cmake
@@ -3,6 +3,7 @@ Type=Application
 Exec=${KDE_INSTALL_FULL_LIBEXECDIR}/kdeconnectd
 X-KDE-StartupNotify=false
 X-KDE-autostart-phase=1
+X-KDE-Wayland-Interfaces=org_kde_kwin_fake_input
 X-GNOME-Autostart-enabled=true
 OnlyShowIn=KDE;GNOME;Unity;XFCE;
 NoDisplay=true
