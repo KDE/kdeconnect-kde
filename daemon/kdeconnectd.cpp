8129db7112a7a917475592eaae7db506dffd9029
Nicolas Fella <nicolas.fella@gmx.de>
2021-03-18 22:03:47 +0100

    Remove <br> from notification text
    The notification spec does not specify that the server must support <br> and thus it can show up literally in the notification. Since we already have a \n we can simply remove it
    
    (cherry picked from commit 5d037396ee4c1244872d91b59d29acaee9882c9f)

--- daemon/kdeconnectd.cpp
+++ daemon/kdeconnectd.cpp
@@ -49,7 +49,7 @@ public:
         notification->setIconName(QStringLiteral("dialog-information"));
         notification->setComponentName(QStringLiteral("kdeconnect"));
         notification->setTitle(QStringLiteral("KDE Connect"));
-        notification->setText(i18n("Pairing request from %1\n<br/>Key: %2...", device->name().toHtmlEscaped(), QString::fromUtf8(device->verificationKey().left(8))));
+        notification->setText(i18n("Pairing request from %1\nKey: %2...", device->name().toHtmlEscaped(), QString::fromUtf8(device->verificationKey().left(8))));
         notification->setDefaultAction(i18n("Open"));
         notification->setActions(QStringList() << i18n("Accept") << i18n("Reject") << i18n("View key"));
         connect(notification, &KNotification::action1Activated, device, &Device::acceptPairing);
