8356cf4e5b0a150af6de720fe5179f8dd621ceed
Piyush Aggarwal <piyushaggarwal002@gmail.com>
2021-06-22 15:24:05 +0530

    systray_actions: add license

--- indicator/systray_actions/systray_actions.h
+++ indicator/systray_actions/systray_actions.h
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2020 Piyush Aggarwal <piyushaggarwal002@gmail.com>
+ *
+ * SPDX-License-Identifier: CC0-1.0 OR LicenseRef-KDE-Accepted-GPL
+ */
+
 #ifndef SYSTRAY_ACTIONS_H
 #define SYSTRAY_ACTIONS_H
 #include <battery_action.h>
