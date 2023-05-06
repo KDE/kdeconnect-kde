8687f8cfdaf689aea5fb2cdb8043c5c4d5a93658
Ali Abdel-Qader <abdelqaderali@protonmail.com>
2023-05-06 15:51:53 -0400

    Use explicit constructor for QSslCertificate with value initialized argument
    Previously the BluetoothDeviceLink::certificate() method was returning a
    value initialized object which I believe default initializes the object.
    However, Clang throws a build error at this because QSslCertificate has
    explicit constructors. This change uses one of those constructors and
    uses value intialization to default construct/initialize the argument
    for it. It fixes the build and hopefully doesn't break anything since
    this is a TODO anyways!
    
    BUG: 469428
    
    Signed-off-by: Ali Abdel-Qader <abdelqaderali@protonmail.com>
    (cherry picked from commit bbac0aa0852b14bc5cc9a1b8c3be0f55adda2428)

--- core/backends/bluetooth/bluetoothdevicelink.cpp
+++ core/backends/bluetooth/bluetoothdevicelink.cpp
@@ -99,5 +99,5 @@ void BluetoothDeviceLink::dataReceived()
 
 QSslCertificate BluetoothDeviceLink::certificate() const
 {
-    return {}; // TODO Not sure what to do here. For LanDeviceLink we use the SSL connection's certificate, but we don't have that here
+    return QSslCertificate({}); // TODO Not sure what to do here. For LanDeviceLink we use the SSL connection's certificate, but we don't have that here
 }
