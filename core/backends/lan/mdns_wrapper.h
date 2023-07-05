611923c24a9f45325ba73b55e788fb6e14079beb
Albert Vaca Cintora <albertvaka@gmail.com>
2023-07-05 21:09:28 +0200

    WIP: Replacing KDNSSD with mdns.h
    KDNSSD only works with Avahi (so, only on Linux) while mdns.h is a
    header-only library [1] that implements mdns from scratch and should
    work on all platforms.
    
    [1] https://github.com/mjansson/mdns

---
+++ core/backends/lan/mdns_wrapper.h
@@ -0,0 +1,50 @@
+/**
+ * SPDX-FileCopyrightText: 2023 Albert Vaca Cintora <albertvaka@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ */
+
+#ifndef KDECONNECT_MDNS_WRAPPER_H
+#define KDECONNECT_MDNS_WRAPPER_H
+
+#include <QHostAddress>
+#include <QMap>
+#include <QSocketNotifier>
+#include <QString>
+#include <QVector>
+
+/*
+ * A Qt wrapper for the mdns.h header-only library
+ * from https://github.com/mjansson/mdns
+ */
+class MdnsWrapper : public QObject
+{
+    Q_OBJECT
+
+public:
+    struct MdnsService {
+        QString serviceName;
+        int port;
+        QHostAddress address;
+        QMap<QString, QString> txtRecords;
+    };
+
+    int startAnnouncing(const char *hostname, const char *service_name, int service_port);
+    void stopAnnouncing();
+
+    void startDiscovering(const char *serviceName);
+    void stopDiscovering();
+
+    void sendQuery(const char *serviceName);
+
+Q_SIGNALS:
+    void serviceFound(const MdnsService &service);
+
+private:
+    int listenForQueryResponses();
+    void stopListeningForQueryResponses();
+
+    QVector<QSocketNotifier *> responseSocketNotifiers;
+};
+
+#endif
