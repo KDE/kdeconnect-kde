b8b30ba571d2c155a09bc7d10fb65338e06ecea9
Albert Vaca Cintora <albertvaka@gmail.com>
2023-08-13 19:43:59 +0000

    Use libdbus instead of GIO to listen for notifications
    This fixes the `Error calling StartServiceByName for org.gtk.vfs.Daemon` message and subsequent crash if both the Receive Notifications plugin and Send Notifications plugin where enabled and we received a notification.
    
    Most of the code comes from the SailfishConnect implementation here: https://github.com/R1tschY/harbour-sailfishconnect/blob/master/plugins/sf_sendnotifications/notificationslistener.cpp

--- plugins/sendnotifications/notificationslistener.h
+++ plugins/sendnotifications/notificationslistener.h
@@ -1,23 +1,42 @@
 /**
  * SPDX-FileCopyrightText: 2015 Holger Kaelberer <holger.k@elberer.de>
+ * SPDX-FileCopyrightText: 2018 Richard Liebscher <richard.liebscher@gmail.com>
  *
  * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
  */
 
 #pragma once
 
-#include <QBuffer>
-#include <QDBusAbstractAdaptor>
-#include <QDBusArgument>
-#include <QFile>
-#include <core/device.h>
-
-#include <gio/gio.h>
+#include <QHash>
+#include <QIODevice>
+#include <QSet>
+#include <QSharedPointer>
+#include <QThread>
+#include <atomic>
+#include <dbus/dbus.h>
 
 class KdeConnectPlugin;
-class Notification;
+
 struct NotifyingApplication;
 
+#define PACKET_TYPE_NOTIFICATION QStringLiteral("kdeconnect.notification")
+
+class NotificationsListenerThread : public QThread
+{
+    Q_OBJECT
+public:
+    void run() override;
+    void stop();
+    void handleNotifyCall(DBusMessage *message);
+
+Q_SIGNALS:
+    void notificationReceived(const QString &, uint, const QString &, const QString &, const QString &, const QStringList &, const QVariantMap &, int);
+
+private:
+    std::atomic<DBusConnection *> m_connection = nullptr;
+};
+
+// TODO: make a singleton, shared for all devices
 class NotificationsListener : public QObject
 {
     Q_OBJECT
@@ -32,7 +51,7 @@ protected:
 
     // virtual helper function to make testing possible (QDBusArgument can not
     // be injected without making a DBUS-call):
-    virtual bool parseImageDataArgument(GVariant *argument,
+    virtual bool parseImageDataArgument(const QVariant &argument,
                                         int &width,
                                         int &height,
                                         int &rowStride,
@@ -40,18 +59,15 @@ protected:
                                         int &channels,
                                         bool &hasAlpha,
                                         QByteArray &imageData) const;
-    QSharedPointer<QIODevice> iconForImageData(GVariant *argument) const;
-    static QSharedPointer<QIODevice> iconForIconName(const QString &iconName);
-
-    static GDBusMessage *onMessageFiltered(GDBusConnection *connection, GDBusMessage *msg, int incoming, void *parent);
+    QSharedPointer<QIODevice> iconForImageData(const QVariant &argument) const;
+    QSharedPointer<QIODevice> iconForIconName(const QString &iconName) const;
+    QSharedPointer<QIODevice> iconFromQImage(const QImage &image) const;
 
 private Q_SLOTS:
     void loadApplications();
+    void onNotify(const QString &, uint, const QString &, const QString &, const QString &, const QStringList &, const QVariantMap &, int);
 
 private:
-    void setTranslatedAppName();
-    QString m_translatedAppName;
-
-    GDBusConnection *m_gdbusConnection = nullptr;
-    unsigned m_gdbusFilterId = 0;
+    QSharedPointer<QIODevice> pngFromImage();
+    NotificationsListenerThread *m_thread;
 };
