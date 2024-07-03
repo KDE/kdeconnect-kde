f41603a4629e20152250478ece5c4c0e03be0a7c
David Redondo <kde@david-redondo.de>
2024-07-03 08:53:03 +0200

    mousepad: On Wayland use ei to send events
    This is to make setting the absolute cursor position work.
    The remote desktop portal only allows screencasting sessions
    to use NotifyPointerMotionAbsolute.

--- plugins/mousepad/waylandremoteinput.h
+++ plugins/mousepad/waylandremoteinput.h
@@ -10,8 +10,12 @@
 #include "abstractremoteinput.h"
 #include "generated/systeminterfaces/remotedesktop.h"
 #include <QDBusObjectPath>
+#include <QSocketNotifier>
 
 class FakeInput;
+class Xkb;
+struct ei;
+struct ei_device;
 
 class RemoteDesktopSession : public QObject
 {
@@ -27,6 +31,13 @@ public:
     QDBusObjectPath m_xdpPath;
     bool m_connecting = false;
 
+    void pointerButton(int button, bool press);
+    void pointerAxis(double dx, double dy);
+    void pointerMotion(double dx, double dy);
+    void pointerMotionAbsolute(double x, double y);
+    void keyboardKeysym(int sym, bool press);
+    void keyboardKeycode(int key, bool press);
+
 private Q_SLOTS:
     void handleXdpSessionCreated(uint code, const QVariantMap &results);
     void handleXdpSessionConfigured(uint code, const QVariantMap &results);
@@ -34,6 +45,14 @@ private Q_SLOTS:
     void handleXdpSessionFinished(uint code, const QVariantMap &results);
 
 private:
+    void connectToEi(int fd);
+    void handleEiEvents();
+    QSocketNotifier m_eiNotifier;
+    std::unique_ptr<Xkb> m_xkb;
+    ei *m_ei = nullptr;
+    ei_device *m_keyboard = nullptr;
+    ei_device *m_pointer = nullptr;
+    ei_device *m_absolutePointer = nullptr;
 };
 
 class WaylandRemoteInput : public AbstractRemoteInput
