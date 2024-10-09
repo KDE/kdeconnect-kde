0e9a4157f28b64e0d9ecd807c6fec7e7855bdd12
Rob Emery <git@mintsoft.net>
2024-10-09 22:00:42 +0100

    Fixing  'KColorSchemeManager::KColorSchemeManager': Since 6.6. Use KColorSchemeManager::instance()

--- app/main.cpp
+++ app/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
     KCrash::initialize();
 
 #ifdef Q_OS_WIN
-    KColorSchemeManager manager;
+    KColorSchemeManager::instance();
     QApplication::setStyle(QStringLiteral("breeze"));
 #endif
 
