d55076cbd90fcd2b0e3de377da9b8d45c9427a76
Albert Vaca Cintora <albertvaka@gmail.com>
2025-05-15 20:58:18 +0200

    Oops, forgot one file

--- app/main.cpp
+++ app/main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 
     KCrash::initialize();
 
-#ifdef Q_OS_WIN
+#if defined(Q_OS_WIN) || defined(Q_OS_MAC)
     // Ensure we have a suitable color theme set for light/dark mode. KColorSchemeManager implicitly applies
     // a suitable default theme.
     KColorSchemeManager::instance();
