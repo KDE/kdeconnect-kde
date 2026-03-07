e23db82ed885d0d28966234a6c6808580dd9c12e
Youssef Al-Bor3y <youssefelboraey02@gmail.com>
2026-03-07 17:55:24 +0200

    Set more appropriate placeholder for repliable notifications
    Since we can now differentiate between different types of notifications,
    let's set a specified placeholder for each one.

--- plugins/notifications/notification.cpp
+++ plugins/notifications/notification.cpp
@@ -113,7 +113,15 @@ void Notification::createKNotification(const NetworkPacket &np)
 
     if (!m_requestReplyId.isEmpty()) {
         auto replyAction = std::make_unique<KNotificationReplyAction>(i18nc("@action:button", "Reply"));
-        replyAction->setPlaceholderText(i18nc("@info:placeholder", "Reply to %1…", m_appName));
+
+        if (isGroupConversation()) {
+            replyAction->setPlaceholderText(i18nc("@info:placeholder", "Reply in %1…", m_groupName));
+        } else {
+            // People usually save others with their full name (e.g. John Doe),
+            // we extract the first name so the placeholder becomes "Reply to John" instead of "Reply to John Doe".
+            replyAction->setPlaceholderText(i18nc("@info:placeholder", "Reply to %1…", isConversation() ? m_title.section(QLatin1Char(' '), 0, 0) : m_appName));
+        }
+
         replyAction->setFallbackBehavior(KNotificationReplyAction::FallbackBehavior::UseRegularAction);
         QObject::connect(replyAction.get(), &KNotificationReplyAction::replied, this, &Notification::replied);
         QObject::connect(replyAction.get(), &KNotificationReplyAction::activated, this, &Notification::reply);
