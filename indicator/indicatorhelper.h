a51c667028db094cf97e03c345f5d30a0f699f93
Albert Vaca Cintora <albertvaka@gmail.com>
2026-04-20 22:43:22 +0200

    Launching the app on non-plasma Linux desktops starts the indicator
    Make the Quit button in the indicator show for all platforms and make it
    kill the daemon (which on Windows, in turn, kills all the other apps)

--- indicator/indicatorhelper.h
+++ indicator/indicatorhelper.h
@@ -13,19 +13,6 @@
 #include <KStatusNotifierItem>
 #endif
 
-#ifdef Q_OS_WIN
-#include <QProcess>
-#include <QUrl>
-namespace processes
-{
-const QString dbus_daemon = QStringLiteral("dbus-daemon.exe");
-const QString kdeconnect_daemon = QStringLiteral("kdeconnectd.exe");
-const QString kdeconnect_app = QStringLiteral("kdeconnect-app.exe");
-const QString kdeconnect_handler = QStringLiteral("kdeconnect-handler.exe");
-const QString kdeconnect_sms = QStringLiteral("kdeconnect-sms.exe");
-};
-#endif
-
 class IndicatorHelper
 {
 public:
@@ -34,26 +21,12 @@ public:
 
     void iconPathHook();
 
-    int startDaemon();
+    void startDaemon();
 
 #ifdef Q_OS_WIN
     void systrayIconHook(QSystemTrayIcon &systray);
 #else
     void systrayIconHook(KStatusNotifierItem &systray);
-#endif
-
-private:
-#ifdef Q_OS_WIN
-    QProcess kdeconnectd;
-
-    /**
-     * Terminate processes of KDE Connect like kdeconnectd.exe and dbus-daemon.exe
-     *
-     * @return True if termination was successful, false otherwise
-     */
-
-    bool terminateProcess(const QString &processName, const QUrl &indicatorUrl) const;
-    QUrl m_indicatorUrl;
 #endif
 };
 
