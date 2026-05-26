b3eaa956884688d8dbdb9b741d30a1895b1675f6
Nicolas Fella <nicolas.fella@gmx.de>
2026-05-26 21:39:18 +0200

    Port to declarative QML type registration
    This vastly improves the support for QML IDE tooling like qmllint

--- declarativeplugin/kdeconnectdeclarativeplugin.cpp
+++ declarativeplugin/kdeconnectdeclarativeplugin.cpp
@@ -16,120 +16,13 @@
 #include "objectfactory.h"
 #include "responsewaiter.h"
 
-#include "core/kdeconnectpluginconfig.h"
-#include "models/commandsmodel.h"
-#include "models/devicesmodel.h"
-#include "models/devicessortproxymodel.h"
-#include "models/notificationsmodel.h"
-#include "openconfig.h"
-#include "pointerlocker.h"
-#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
-#include "pointerlockerwayland.h"
-#endif
-#include <pluginmodel.h>
-#include <remotecommandsmodel.h>
-#include <remotesinksmodel.h>
-
 QObject *createDBusResponse()
 {
     return new DBusAsyncResponse();
 }
 
-template<typename T>
-void registerFactory(const char *uri, const char *name)
+void KdeConnectDeclarativePlugin::registerTypes(const char * /*uri*/)
 {
-    qmlRegisterSingletonType<ObjectFactory>(uri, 1, 0, name, [](QQmlEngine *engine, QJSEngine *) -> QObject * {
-        return new ObjectFactory(engine, [](const QVariant &deviceId) -> QObject * {
-            return new T(deviceId.toString());
-        });
-    });
-}
-
-void KdeConnectDeclarativePlugin::registerTypes(const char *uri)
-{
-    qmlRegisterType<DevicesModel>(uri, 1, 0, "DevicesModel");
-    qmlRegisterType<NotificationsModel>(uri, 1, 0, "NotificationsModel");
-    qmlRegisterType<RemoteCommandsModel>(uri, 1, 0, "RemoteCommandsModel");
-    qmlRegisterType<DBusAsyncResponse>(uri, 1, 0, "DBusAsyncResponse");
-    qmlRegisterType<DevicesSortProxyModel>(uri, 1, 0, "DevicesSortProxyModel");
-    qmlRegisterType<DevicesPluginFilterProxyModel>(uri, 1, 0, "DevicesPluginFilterProxyModel");
-    qmlRegisterType<RemoteSinksModel>(uri, 1, 0, "RemoteSinksModel");
-    qmlRegisterType<PluginModel>(uri, 1, 0, "PluginModel");
-    qmlRegisterType<KdeConnectPluginConfig>(uri, 1, 0, "KdeConnectPluginConfig");
-    qmlRegisterType<CommandsModel>(uri, 1, 0, "CommandsModel");
-    qmlRegisterUncreatableType<MprisDbusInterface>(uri, 1, 0, "MprisDbusInterface", QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<LockDeviceDbusInterface>(uri, 1, 0, "LockDeviceDbusInterface", QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<FindMyPhoneDeviceDbusInterface>(uri,
-                                                               1,
-                                                               0,
-                                                               "FindMyPhoneDbusInterface",
-                                                               QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<ClipboardDbusInterface>(uri, 1, 0, "ClipboardDbusInterface", QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<RemoteKeyboardDbusInterface>(uri,
-                                                            1,
-                                                            0,
-                                                            "RemoteKeyboardDbusInterface",
-                                                            QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<DeviceDbusInterface>(uri, 1, 0, "DeviceDbusInterface", QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<BatteryDbusInterface>(uri, 1, 0, "BatteryDbusInterface", QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<ConnectivityReportDbusInterface>(uri,
-                                                                1,
-                                                                0,
-                                                                "ConnectivityReportDbusInterface",
-                                                                QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<SftpDbusInterface>(uri, 1, 0, "SftpDbusInterface", QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<SmsDbusInterface>(uri, 1, 0, "SmsDbusInterface", QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<VirtualmonitorDbusInterface>(uri,
-                                                            1,
-                                                            0,
-                                                            "VirtualmonitorDbusInterface",
-                                                            QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<RemoteCommandsDbusInterface>(uri,
-                                                            1,
-                                                            0,
-                                                            "RemoteCommandsDbusInterface",
-                                                            QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<RemoteSystemVolumeDbusInterface>(uri,
-                                                                1,
-                                                                0,
-                                                                "RemoteSystemVolumeInterface",
-                                                                QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterUncreatableType<ShareDbusInterface>(uri, 1, 0, "ShareDbusInterface", QStringLiteral("You're not supposed to instantiate interfaces"));
-    qmlRegisterSingletonType<DaemonDbusInterface>(uri, 1, 0, "DaemonDbusInterface", [](QQmlEngine *, QJSEngine *) -> QObject * {
-        return new DaemonDbusInterface;
-    });
-    qmlRegisterSingletonType<AbstractPointerLocker>("org.kde.kdeconnect", 1, 0, "PointerLocker", [](QQmlEngine *, QJSEngine *) -> QObject * {
-        AbstractPointerLocker *ret;
-#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
-        if (qGuiApp->platformName() == QLatin1String("wayland"))
-            ret = new PointerLockerWayland;
-        else
-#endif
-            ret = new PointerLockerQt;
-        return ret;
-    });
-
-    qmlRegisterSingletonType<OpenConfig>(uri, 1, 0, "OpenConfig", [](QQmlEngine *, QJSEngine *) -> QObject * {
-        return new OpenConfig;
-    });
-
-    qmlRegisterAnonymousType<QAbstractItemModel>(uri, 1);
-
-    registerFactory<DeviceDbusInterface>(uri, "DeviceDbusInterfaceFactory");
-    registerFactory<BatteryDbusInterface>(uri, "DeviceBatteryDbusInterfaceFactory");
-    registerFactory<ConnectivityReportDbusInterface>(uri, "DeviceConnectivityReportDbusInterfaceFactory");
-    registerFactory<FindMyPhoneDeviceDbusInterface>(uri, "FindMyPhoneDbusInterfaceFactory");
-    registerFactory<SftpDbusInterface>(uri, "SftpDbusInterfaceFactory");
-    registerFactory<RemoteKeyboardDbusInterface>(uri, "RemoteKeyboardDbusInterfaceFactory");
-    registerFactory<ClipboardDbusInterface>(uri, "ClipboardDbusInterfaceFactory");
-    registerFactory<MprisDbusInterface>(uri, "MprisDbusInterfaceFactory");
-    registerFactory<RemoteControlDbusInterface>(uri, "RemoteControlDbusInterfaceFactory");
-    registerFactory<LockDeviceDbusInterface>(uri, "LockDeviceDbusInterfaceFactory");
-    registerFactory<SmsDbusInterface>(uri, "SmsDbusInterfaceFactory");
-    registerFactory<RemoteCommandsDbusInterface>(uri, "RemoteCommandsDbusInterfaceFactory");
-    registerFactory<ShareDbusInterface>(uri, "ShareDbusInterfaceFactory");
-    registerFactory<RemoteSystemVolumeDbusInterface>(uri, "RemoteSystemVolumeDbusInterfaceFactory");
-    registerFactory<VirtualmonitorDbusInterface>(uri, "VirtualmonitorDbusInterfaceFactory");
 }
 
 void KdeConnectDeclarativePlugin::initializeEngine(QQmlEngine *engine, const char *uri)
--- declarativeplugin/kdeconnectdeclarativeplugin.h
+++ declarativeplugin/kdeconnectdeclarativeplugin.h
@@ -7,7 +7,431 @@
 #ifndef KDECONNECTDECLARATIVEPLUGIN_H
 #define KDECONNECTDECLARATIVEPLUGIN_H
 
+#include <QGuiApplication>
+#include <QQmlEngine>
 #include <QQmlExtensionPlugin>
+
+#include <qqmlregistration.h>
+
+#include "commandsmodel.h"
+#include "dbusinterfaces.h"
+#include "devicesmodel.h"
+#include "devicespluginfilterproxymodel.h"
+#include "devicessortproxymodel.h"
+#include "kdeconnectpluginconfig.h"
+#include "notificationsmodel.h"
+#include "openconfig.h"
+#include "pluginmodel.h"
+#include "pointerlocker.h"
+#include "remotecommandsmodel.h"
+#include "remotesinksmodel.h"
+#include "responsewaiter.h"
+
+#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
+#include "pointerlockerwayland.h"
+#endif
+
+class KdeConnectPluginConfigForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(KdeConnectPluginConfig)
+    QML_FOREIGN(KdeConnectPluginConfig)
+};
+
+class DBusAsyncResponseForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(DBusAsyncResponse)
+    QML_FOREIGN(DBusAsyncResponse)
+};
+
+class CommandsModelForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(CommandsModel)
+    QML_FOREIGN(CommandsModel)
+};
+
+class PluginModelForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(PluginModel)
+    QML_FOREIGN(PluginModel)
+};
+
+class DevicesModelForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(DevicesModel)
+    QML_FOREIGN(DevicesModel)
+};
+
+class DevicesSortProxyModelForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(DevicesSortProxyModel)
+    QML_FOREIGN(DevicesSortProxyModel)
+};
+
+class DevicesPluginFilterProxyModelForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(DevicesPluginFilterProxyModel)
+    QML_FOREIGN(DevicesPluginFilterProxyModel)
+};
+
+class NotificationsModelForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(NotificationsModel)
+    QML_FOREIGN(NotificationsModel)
+};
+
+class RemoteCommandsModelForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(RemoteCommandsModel)
+    QML_FOREIGN(RemoteCommandsModel)
+};
+
+class RemoteSinksModelForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(RemoteSinksModel)
+    QML_FOREIGN(RemoteSinksModel)
+};
+
+class BatteryDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(BatteryDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(BatteryDbusInterface)
+};
+
+class DeviceDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(DeviceDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(DeviceDbusInterface)
+};
+
+class ConnectivityReportDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(ConnectivityReportDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(ConnectivityReportDbusInterface)
+};
+
+class ShareDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(ShareDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(ShareDbusInterface)
+};
+
+class VirtualmonitorDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(VirtualmonitorDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(VirtualmonitorDbusInterface)
+};
+
+class SftpDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(SftpDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(SftpDbusInterface)
+};
+
+class ClipboardDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(ClipboardDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(ClipboardDbusInterface)
+};
+
+class RemoteKeyboardDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(RemoteKeyboardDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(RemoteKeyboardDbusInterface)
+};
+
+class MprisDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(MprisDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(MprisDbusInterface)
+};
+
+class LockDeviceDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(LockDeviceDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(LockDeviceDbusInterface)
+};
+
+class FindMyPhoneDeviceDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(FindMyPhoneDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(FindMyPhoneDeviceDbusInterface)
+};
+
+class SmsDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(SmsDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(SmsDbusInterface)
+};
+
+class RemoteCommandsDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(RemoteCommandsDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(RemoteCommandsDbusInterface)
+};
+
+class RemoteSystemVolumeDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(RemoteSystemVolumeDbusInterface)
+    QML_UNCREATABLE("")
+    QML_FOREIGN(RemoteSystemVolumeDbusInterface)
+};
+
+class DaemonDbusInterfaceForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(DaemonDbusInterface)
+    QML_SINGLETON
+    QML_FOREIGN(DaemonDbusInterface)
+};
+
+class OpenConfigForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(OpenConfig)
+    QML_SINGLETON
+    QML_FOREIGN(OpenConfig)
+};
+
+class DeviceDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE DeviceDbusInterface *create(const QString &deviceId)
+    {
+        return new DeviceDbusInterface(deviceId);
+    }
+};
+
+class DeviceBatteryDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE BatteryDbusInterface *create(const QString &deviceId)
+    {
+        return new BatteryDbusInterface(deviceId);
+    }
+};
+
+class DeviceConnectivityReportDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE ConnectivityReportDbusInterface *create(const QString &deviceId)
+    {
+        return new ConnectivityReportDbusInterface(deviceId);
+    }
+};
+
+class ShareDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE ShareDbusInterface *create(const QString &deviceId)
+    {
+        return new ShareDbusInterface(deviceId);
+    }
+};
+
+class VirtualmonitorDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE VirtualmonitorDbusInterface *create(const QString &deviceId)
+    {
+        return new VirtualmonitorDbusInterface(deviceId);
+    }
+};
+
+class SftpDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE SftpDbusInterface *create(const QString &deviceId)
+    {
+        return new SftpDbusInterface(deviceId);
+    }
+};
+
+class SmsDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE SmsDbusInterface *create(const QString &deviceId)
+    {
+        return new SmsDbusInterface(deviceId);
+    }
+};
+
+class ClipboardDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE ClipboardDbusInterface *create(const QString &deviceId)
+    {
+        return new ClipboardDbusInterface(deviceId);
+    }
+};
+
+class RemoteSystemVolumeDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE RemoteSystemVolumeDbusInterface *create(const QString &deviceId)
+    {
+        return new RemoteSystemVolumeDbusInterface(deviceId);
+    }
+};
+
+class RemoteKeyboardDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE RemoteKeyboardDbusInterface *create(const QString &deviceId)
+    {
+        return new RemoteKeyboardDbusInterface(deviceId);
+    }
+};
+
+class RemoteControlDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE RemoteControlDbusInterface *create(const QString &deviceId)
+    {
+        return new RemoteControlDbusInterface(deviceId);
+    }
+};
+
+class LockDeviceDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE LockDeviceDbusInterface *create(const QString &deviceId)
+    {
+        return new LockDeviceDbusInterface(deviceId);
+    }
+};
+
+class FindMyPhoneDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE FindMyPhoneDeviceDbusInterface *create(const QString &deviceId)
+    {
+        return new FindMyPhoneDeviceDbusInterface(deviceId);
+    }
+};
+
+class RemoteCommandsDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE RemoteCommandsDbusInterface *create(const QString &deviceId)
+    {
+        return new RemoteCommandsDbusInterface(deviceId);
+    }
+};
+
+class MprisDbusInterfaceFactory : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+public:
+    Q_INVOKABLE MprisDbusInterface *create(const QString &deviceId)
+    {
+        return new MprisDbusInterface(deviceId);
+    }
+};
+
+class AbstractPointerLockerForeign
+{
+    Q_GADGET
+    QML_NAMED_ELEMENT(PointerLocker)
+    QML_FOREIGN(AbstractPointerLocker)
+    QML_SINGLETON
+
+public:
+    static AbstractPointerLocker *create(QQmlEngine *, QJSEngine *)
+    {
+        AbstractPointerLocker *ret;
+#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
+        if (qGuiApp->platformName() == QLatin1String("wayland"))
+            ret = new PointerLockerWayland;
+        else
+#endif
+            ret = new PointerLockerQt;
+        return ret;
+    }
+
+private:
+    AbstractPointerLockerForeign() = default;
+};
 
 class KdeConnectDeclarativePlugin : public QQmlExtensionPlugin
 {
