c0d17c854546e15332123c62c9d049b157f545e8
Logan Rosen <loganrosen@gmail.com>
2026-07-26 21:30:22 +0000

    macos: focus app when opened from indicator

---
+++ app/macosforegroundapp.h
@@ -0,0 +1,11 @@
+/**
+ * SPDX-FileCopyrightText: 2026 KDE Connect Team
+ *
+ * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ */
+
+#pragma once
+
+class QWindow;
+
+void activateWindowForMacOS(QWindow *window);
---
+++ app/macosforegroundapp.mm
@@ -0,0 +1,24 @@
+/**
+ * SPDX-FileCopyrightText: 2026 KDE Connect Team
+ *
+ * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+ */
+
+#include "macosforegroundapp.h"
+
+#include <QWindow>
+
+#import <AppKit/NSApplication.h>
+
+void activateWindowForMacOS(QWindow *window)
+{
+    if (window == nullptr) {
+        return;
+    }
+
+    [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+    [NSApp activateIgnoringOtherApps:YES];
+    window->show();
+    window->raise();
+    window->requestActivate();
+}
