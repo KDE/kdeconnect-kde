2c56134a962ca1b30d058a56f16faeb1a9e8789b
Albert Vaca Cintora <albertvaka@gmail.com>
2026-07-26 23:33:05 +0200

    Fix copyright header

--- app/macosforegroundapp.h
+++ app/macosforegroundapp.h
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: 2026 KDE Connect Team
+ * SPDX-FileCopyrightText: 2026 Logan Rosen <loganrosen@gmail.com>
  *
  * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
  */
